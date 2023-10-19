8a83f25db2ffd3d84f35f576e068d2ba8efc9760
Oskar <moreez@protonmail.com>
2023-10-19 12:30:47 +0100

    Removed unnecessary code.

--- server/server.js
+++ server/server.js
@@ -1,28 +1,21 @@
-import { createServer } from "http";
 import express from "express";
-import compression from "compression";
 import { dirname, join } from "path";
-import { fileURLToPath } from "url";
+import { fileURLToPath } from "url";  
 const __dirname = dirname(fileURLToPath(import.meta.url));
-import bodyParser from "body-parser";
 import api from "./api.js"; // Assuming 'api.js' has been converted to ES Modules
 import dotenv from 'dotenv';
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
-
-app.use(compression());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(join(__dirname, "../client/dist")));
 app.use(api);
 
 app.get("/", function (req, res) {
-  res.sendFile(join(__dirname, "index.html"));
+  res.send("Failed to compile vue build");
 });
 
-var server = createServer(app);
-server.listen(port);
-console.log("Server is running at port: " + port);
+app.listen(PORT, ()=>{
+  console.log("Server is running at port: " + PORT);
+});
